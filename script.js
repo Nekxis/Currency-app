f1f250ff8eadb4f3637d3f2af15359bcae216e90
Nekxis <95935590+Nekxis@users.noreply.github.com>
2022-05-01 01:10:48 +0200

    7

--- script.js
+++ script.js
@@ -2,34 +2,55 @@ const openBtn = document.querySelector('.open');
 const closeBtn = document.querySelector('.close');
 const menuContainer = document.querySelector('.menu-container');
 const menuList = document.querySelector('.menu-list');
-const searchBox = document.querySelector('.search-box')
-const searchInput = document.querySelector('.text-field')
-const clearButton = document.querySelector('.clear-input')
+const inputContainer = document.querySelector('.input-container');
+const searchInput = document.querySelector('.text-field');
+const clearButton = document.querySelector('.clear-input');
+const apiKey = '4fea2ddd350e4b5397126f3b42e462b2'
+const url = `https://api.currencyfreaks.com/latest?apikey=${apiKey}`
 
-
-
-
+const fetchAPI = () => {
+    fetch(url)
+        .then((res) => {console.log(res.json())})
+}
 
 const openUp = () => {
     menuContainer.classList.add('active');
     openBtn.style.display = 'none';
     closeBtn.style.display = 'block';
-    menuList.classList.remove('close-menu-list')
-}
+    menuList.classList.remove('close-menu-list');
+};
 const closeUp = () => {
     menuContainer.classList.remove('active');
     openBtn.style.display = 'block';
     closeBtn.style.display = 'none';
     menuList.classList.add('close-menu-list')
-}
+};
+
 const inputClear = () => {
+   clearButton.addEventListener('click', () => {
+       searchInput.value = '';
+       clearButton.style.display = 'none';
+       inputContainer.style.borderBottomColor = '#d1d1d1';
+   })
+};
+const inputActive = () => {
     if (searchInput.value !== ''){
         clearButton.style.display = 'inline-block';
+        inputContainer.style.borderBottomColor = '#000';
     } else {
         clearButton.style.display = 'none';
     }
+    inputClear();
 }
-
 openBtn.addEventListener('click', openUp);
 closeBtn.addEventListener('click', closeUp);
-searchInput.addEventListener('keyup', inputClear);
+searchInput.addEventListener('keyup', inputActive);
+document.addEventListener('click', (e) =>{
+    if (e.target === searchInput) {
+        inputContainer.style.borderBottomColor = '#000';
+    } else {
+        inputContainer.style.borderBottomColor = '#d1d1d1';
+    }
+});
+document.addEventListener('DOMContentLoaded', fetchAPI)
+
