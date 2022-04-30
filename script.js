71365badbdebea132fca22a0163b72deaea6dd6c
Nekxis <95935590+Nekxis@users.noreply.github.com>
2022-05-01 00:15:13 +0200

    6

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const openBtn = document.querySelector('.open');
 const closeBtn = document.querySelector('.close');
 const menuContainer = document.querySelector('.menu-container');
 const menuList = document.querySelector('.menu-list');
+const searchBox = document.querySelector('.search-box')
+const searchInput = document.querySelector('.text-field')
+const clearButton = document.querySelector('.clear-input')
+
+
+
+
 
 const openUp = () => {
     menuContainer.classList.add('active');
@@ -15,6 +22,14 @@ const closeUp = () => {
     closeBtn.style.display = 'none';
     menuList.classList.add('close-menu-list')
 }
+const inputClear = () => {
+    if (searchInput.value !== ''){
+        clearButton.style.display = 'inline-block';
+    } else {
+        clearButton.style.display = 'none';
+    }
+}
 
 openBtn.addEventListener('click', openUp);
 closeBtn.addEventListener('click', closeUp);
+searchInput.addEventListener('keyup', inputClear);
